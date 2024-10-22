67694a8138770d16f956259694462006b9474bb7
socomja <ansunjin1@gmail.com>
2024-10-22 17:20:19 +0900

    뉴스 스와이퍼

---
+++ 스텔/script/news.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var swiper = new Swiper('.swiper-container', {
+        loop: true, // 슬라이드 반복
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true, // 페이지네이션 클릭 가능하게 설정
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        autoplay: {
+            delay: 3000, // 3초마다 자동 슬라이드 전환
+        },
+    });
+});
