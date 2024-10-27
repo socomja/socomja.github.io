2c536d38b9385c65fa70591e66f98b30a0323e66
socomja <ansunjin1@gmail.com>
2024-10-28 00:35:10 +0900

    스와이퍼 버그

--- 스텔/script/news.js
+++ 스텔/script/news.js
@@ -1,22 +1,23 @@
 
-$(() => {
-    document.addEventListener('DOMContentLoaded', function () {
-        var swiper = new Swiper('.swiper-container', {
-            speed: 750,
-            loop: true, // 슬라이드 반복
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true, // 페이지네이션 클릭 가능하게 설정
-            },
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-            },
-            autoplay: {
-                delay: 3000, // 3초마다 자동 슬라이드 전환
-            },
-        });
+document.addEventListener('DOMContentLoaded', function () {
+    var swiper = new Swiper('.swiper-container', {
+        speed: 750,
+        loop: true, // 슬라이드 반복
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true, // 페이지네이션 클릭 가능하게 설정
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        autoplay: {
+            delay: 3000, // 3초마다 자동 슬라이드 전환
+        },
     });
+});
+
+$(() => {
 
     // 모든 버튼과 카드 요소들을 선택
     const buttons = document.querySelectorAll(".news-tab a");
