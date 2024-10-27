cac9869131ae555c604c177d75f78684b7186c77
socomja <ansunjin1@gmail.com>
2024-10-28 01:13:08 +0900

    .

--- 스텔/script/news.js
+++ 스텔/script/news.js
@@ -2,25 +2,60 @@
 document.addEventListener('DOMContentLoaded', function () {
     var swiper = new Swiper('.swiper-container', {
         speed: 750,
-        loop: true, // 슬라이드 반복
+        loop: true,
         pagination: {
             el: '.swiper-pagination',
-            clickable: true, // 페이지네이션 클릭 가능하게 설정
+            clickable: true,
         },
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
         autoplay: {
-            delay: 3000, // 3초마다 자동 슬라이드 전환
+            delay: 3000,
         },
     });
 });
 
 $(() => {
+    AOS.init();
+
+    $('.btn-more').each(function () {
+        const $btnMore = $(this);
+        const $path = $btnMore.find('.btn_line');
+        const $circle = $btnMore.find('.move');
+        const moveSpeed = 0.005;
+
+        let animationId;
+        let currentPoint = 0;
+
+        function updatePosition() {
+            currentPoint = (currentPoint + moveSpeed) % 1;
+            const pathLength = $path[0].getTotalLength();
+            const point = $path[0].getPointAtLength(currentPoint * pathLength);
+
+            $circle.attr({
+                cx: point.x,
+                cy: point.y
+            });
+
+            animationId = requestAnimationFrame(updatePosition);
+        }
+
+        $btnMore.on('mouseover', updatePosition);
+        $btnMore.on('mouseout', () => {
+            if (animationId) {
+                cancelAnimationFrame(animationId);
+                animationId = undefined;
+            }
+        });
+    });
+
+
     const buttons = document.querySelectorAll(".news-tab a");
     const cards = document.querySelectorAll(".news-card li");
     const itemCount = document.querySelector(".item_count span");
+    const initialVisibleCount = 4;
 
     function updateItemCount(count) {
         itemCount.textContent = `총 ${count}건`;
